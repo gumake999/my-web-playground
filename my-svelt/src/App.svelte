3288499bcb6d9f2c805417e73620998a2e21a4ad
Grey <grey@teamo2.kr>
2023-12-26 20:43:21 +0900

    test

--- my-svelt/src/App.svelte
+++ my-svelt/src/App.svelte
@@ -5,6 +5,7 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>정말 되는건지 테스트!!!!!!!!!!</p>
 </main>
 
 <style>
