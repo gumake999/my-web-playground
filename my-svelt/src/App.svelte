350093d62d61a0bbd410953e8542dfd47188ee2b
Grey <grey@teamo2.kr>
2023-12-26 21:08:23 +0900

    FEAT: 1. todo list

--- my-svelt/src/App.svelte
+++ my-svelt/src/App.svelte
@@ -1,31 +1,26 @@
 <script>
-	export let name;
+  import Todo from './Todo.svelte';
+  let title = ''
+  let todos = []
+  let id = 0
+
+  function createTodo() {
+    todos.push({
+      id,
+      title,
+    });
+    todos = todos
+
+    title = ''
+    id += 1
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<p>정말 되는건지 테스트!!!!!!!!!!</p>
-</main>
+<input type="text" bind:value={title}/>
+<button type="button" on:click={createTodo}>
+  Create To Do
+</button>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+{#each todos as todo}
+  <Todo {todo} />
+{/each}
